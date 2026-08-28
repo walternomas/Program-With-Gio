d87c9d007026e0a4983b56fce94ac91292b24fc5
Walter Barrios <walter.barrios@nerdo.solutions>
2026-08-28 20:16:14 -0300

    Add welcome

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  $name = "Gio";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program With <?php echo $name; ?>!</title>
+  </head>
+
+  <body>
+    <h1>
+      Welcome to Program With <?= $name ?>!
+    </h1>
+    <p>
+      This is a simple PHP program that greets the user.
+    </p>
+  </body>
+
+</html>
