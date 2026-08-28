c272f813a6fdad9c1a835de1b8c9c231981dcf85
Walter Barrios <walter.barrios@nerdo.solutions>
2026-08-28 20:27:21 -0300

    Add constants

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+
+// Constants
+
+define("FIRST_NAME", "Gio"); // Define a constant named FIRST_NAME with the value "Gio"
+
+// FIRST_NAME = "Joe"; // This will cause an error because constants cannot be redefined
+
+echo FIRST_NAME; // Output: Gio
