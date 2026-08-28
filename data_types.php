82f2f554670d0dd41993626c9fba3c5ef1777aa5
Walter Barrios <walter.barrios@nerdo.solutions>
2026-08-28 20:58:21 -0300

    Add data types

---
+++ data_types.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Data Types & Type Casting in PHP */
+
+# 4 Scalar Types
+  # bool - true / false
+  # int - whole numbers
+  # float - decimal numbers
+  # string - sequence of characters
+
+# 4 Compound Types
+  # array - collection of values
+  # object - instance of a class
+  # callable - a callback function
+  # iterable - a value that can be looped over with foreach
+
+# 2 Special Types
+  # resource - a reference to an external resource
+  # NULL - a variable with no value
